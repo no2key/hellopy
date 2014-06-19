0307c7bcf4656432200a8b29b1d1cc68d22ff83b
maguowei <imaguowei@gmail.com>
2014-06-19 16:55:15 +0800

    update

--- supervisorkit/confcc.py
+++ supervisorkit/confcc.py
@@ -3,18 +3,39 @@
 
 
 class Supervisor():
-    def __init__(self, group, program, port, num):
+    def __init__(self, group, program, port, num, directory, command):
         self.group = group
         self.program = program
         self.port = port
         self.num = num
+        self.directory = directory
+        self.command = command
         self.programs = []
 
     def mk_group(self):
-        return '[group:{}]'.format(self.group)
+        return '[group:{}]\n'.format(self.group)
 
     def mk_programs(self):
         res = ('programs=' + '{}, ' * self.num)[:-2]
         for i in range(self.num):
             self.programs.append(self.program + '-' + str(self.port + i))
-        return res.format(*self.programs)
+        return res.format(*self.programs) + '\n'
+
+    def mk_program(self, port):
+        res = """
+[program:{program}-{port}]
+command=python3 {directory}/{command} --port={port}
+directory={directory}
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/var/log/{program}/{program}@{port}.log
+loglevel=info
+        """
+        return res.format(program=self.program, port=port, directory=self.directory, command=self.command)
+
+    def get_all_programs(self):
+        res = []
+        for i in range(self.num):
+            res.append(self.mk_program(self.port + i))
+
+        return res
--- supervisorkit/test.py
+++ supervisorkit/test.py
@@ -5,7 +5,7 @@ from supervisorkit.confcc import Supervisor
 class TestSupervisor(unittest.TestCase):
 
     def setUp(self):
-        self.supervisor = Supervisor('tornados', 'tornado', 8000, 4)
+        self.supervisor = Supervisor('tornados', 'tornado', 8000, 4, '/home/maguowei/tornado', 'bastogne.py')
 
     def test_mk_group(self):
         self.assertEqual('[group:tornados]', self.supervisor.mk_group())
@@ -13,3 +13,24 @@ class TestSupervisor(unittest.TestCase):
     def test_mk_programs(self):
         self.assertEqual('programs=tornado-8000, tornado-8001, tornado-8002, tornado-8003', self.supervisor.mk_programs())
         self.assertListEqual(['tornado-8000', 'tornado-8001', 'tornado-8002', 'tornado-8003'], self.supervisor.programs)
+
+    def test_mk_program(self):
+        res = """
+[program:tornado-8000]
+command=python3 /home/maguowei/tornado/bastogne.py --port=8000
+directory=/home/maguowei/tornado
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/var/log/tornado/tornado@8000.log
+loglevel=info
+        """
+        self.assertEqual(res, self.supervisor.mk_program(8000))
+
+    def test_get_all_programs(self):
+        with open('supervisor.conf', 'w') as f:
+            f.write(self.supervisor.mk_group())
+            f.write(self.supervisor.mk_programs())
+            for p in self.supervisor.get_all_programs():
+                f.write(p)
+
+        self.assertListEqual([], self.supervisor.get_all_programs())
