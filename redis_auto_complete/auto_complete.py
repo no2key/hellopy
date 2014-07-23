a595602a98e0d8c3ce0703fd4c38cf52e15d4ed8
maguowei <imaguowei@gmail.com>
2014-07-23 23:16:44 +0800

    更新算法

--- redis_auto_complete/auto_complete.py
+++ redis_auto_complete/auto_complete.py
@@ -19,36 +19,42 @@ class AutoCpmplate():
         for i in l:
             self.add(i)
 
-    # def add(self, d):
-    #     for i in range(1, len(d[1])+1):
-    #         # self.r.sadd(d[1][0:i], d[2])
-    #         self.r.sadd(d[1][0:i], d[1])
-    #         # print(d[1][0:i])
+    def get_keys(s):
+        data = []
+        for i in range(0, len(s)):
+            for j in range(i+1, len(s)+1):
+                data.append(s[i:j])
+
+        return data
 
     def add(self, s):
         keys = self.get_keys(s)
         for i in keys:
             self.r.sadd(i, s)
 
-    def get_keys(self, s):
-        """ 取得所有键
-        对于输入'abc', 返回[a,b,c,ab,ac,bc,abc]
-
-        :param s:
-        :return:
-        """
-        data = []
-        for i in range(1, len(s)+1):
-            keys = list(itertools.combinations(s, i))
-            for key in keys:
-                data.append(reduce(lambda x, y: x+y, key))
-
-        return data
+    # def get_keys(self, s):
+    #     """ 取得所有键
+    #     对于输入'abc', 返回[a,b,c,ab,ac,bc,abc]
+    #
+    #     :param s:
+    #     :return:
+    #     """
+    #     data = []
+    #     for i in range(1, len(s)+1):
+    #         keys = list(itertools.combinations(s, i))
+    #         for key in keys:
+    #             data.append(''.join(key))
+    #     return data
 
 
 if __name__ == '__main__':
     auto = AutoCpmplate()
-    test_data = ['张三', '张三丰', '张学良']
-    auto.add_all(test_data)
-    for j in auto.r.sdiff('三'):
-        print(j.decode())
+    # test_data = ['张三', '张三丰', '张学良']
+    # auto.add_all(test_data)
+    # for j in auto.r.sdiff('三'):
+    #     print(j.decode())
+
+    # res = auto.get_keys('abc')
+    # print(res)
+
+    print(auto.get_keys('abc'))
